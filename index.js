822996e9b6b029e74c136786b28c5d7e4121ef56
nayem-upo <nayemislam0181@gmail.com>
2023-05-26 11:00:22 +0600

    updated midlewares

--- index.js
+++ index.js
@@ -7,6 +7,8 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 //midlewares
+app.use(cors());
+app.use(express.json());
 
 
 const uri = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.obhdclo.mongodb.net/?retryWrites=true&w=majority`;
